f1db858f9fa00c06ac19b389031c405ee703f81c
Jonathan Frassineti <jonathan.frassineti2@unibo.it>
2022-06-10 11:33:35 +0200

    Updated .py files.

--- Crystalline_structure.py
+++ Crystalline_structure.py
@@ -1,27 +1,58 @@
-# In this file, the user can add the basis that forms the crystalline structure of the material
-# in which you can calculate the Electric Field Gradient (EFG) by the Point Charge approximation
-# The following is an example with V3Si, a well known superconductor with cubic structure.
+# -*- coding: utf-8 -*-
+"""
+@author: Jonathan Frassineti
+"""
+
+"""
+In this file, the user can add the basis that forms the crystalline structure of the material
+in which you can calculate the Electric Field Gradient (EFG) by the Point Charge approximation
+The following is an example with V3Si, a well known superconductor with cubic structure.
+"""
 
 import numpy as np
 
-atomic_EFG = - 9.71736166e21 # V/(m^2), atomic unit of EFG
-angtom = 1.0e-10 # Angstrom unit of measure
-elementary_charge = 1.6021766e-19 # Coulomb = ampere*second
-epsilon0 = 8.8541878e-12 # dielectric constant of the vacuum, in ampere^2*kilogram^−1*meter^−3*second^4
-h = 6.6260693e-34 # J*s
-hbar = h/(2*np.pi) # J*s
+""" 
+Now, the atomic basis is added. Each row consists of an atom with:
+[x position, y position, z position, occupation, electric charge]
+The user must add every atom that forms the basis.
+"""
 
-# Now the basis is added. Each row consists of an atom with:
-# [x position, y position, z position, occupation, electric charge]
-# The user must add every atom that forms the basis.
+base = np.array([[0.25, 0.75, 0.00, 1, 1], # V, along a axis
+                 [0.75, 0.75, 0.00, 1, 1], # //
+                 [0.50, 0.50, 0.50, 1, 1], # V, along b axis
+                 [0.50, 0.00, 0.50, 1, 1], # //
+                 [0.00, 0.25, 0.75, 1, 1], # V, along c axis
+                 [0.00, 0.25, 0.25, 1, 1], # //
+                 [0.00, 0.75, 0.50, 1, -3],   # Si
+                 [0.50, 0.25, 0.00, 1, -3]])  # Si
 
-base = np.array([[0.25, 0.00, 0.5, 1, 1], # V, along a axis
-                 [0.75, 0.00, 0.5, 1, 1], # //
-                 [0.50, 0.25, 0.00, 1, 1], # V, along b axis
-                 [0.50, 0.75, 0.00, 1, 1], # //
-                 [0.00, 0.50, 0.75, 1, 1], # V, along c axis
-                 [0.00, 0.50, 0.25, 1, 1], # //
-                 [0.00, 0.00, 0.00, 1, -3],   # Si
-                 [0.50, 0.50, 0.50, 1, -3]])  # Si
+lattice_par1 = 4.722 # Lattice parameter 'a' of V3Si, in Angstrom
+lattice_par2 = 4.722 # Lattice parameter 'b' of V3Si, in Angstrom
+lattice_par3 = 4.722 # Lattice parameter 'c' of V3Si, in Angstrom
 
+quadrupole_moment = -0.052e-28 # Quadrupolar moment of the atom (in this case, 51V), in m^2
 
+"""
+Now, the possibility to add an impurity, or vacancy, or an additional particle
+(such as a positive muon) inside the crystal, to obtain the EFG with this perturbation.
+First, define the position of the impurity, the atomic base of N nearest neighbours 
+(for this version, only 4 considered) which are displaced due to the presence of the impurity, the displacement itself
+and the list of N nearest neighbours involved from the original atomic base
+(used for EFG calculations).
+"""
+xxx = np.array([0,0,0,1,1]) # Same as for V or Si atom, in this case a H atom at (0,0,0) position
+
+shift_a = 1.05 # The displacement of N nearest neighbours 
+               # due to the presence of 'xxx' impurity along 'a'
+shift_b = 1.05 # Same, along 'b'
+shift_c = 1.05 # Same, along 'c'
+
+shifts = np.array([shift_a,shift_b,shift_c])
+
+base_shifted = np.array([[ 0.25*shift_a, -0.25*shift_b,  0.00*shift_c, 1, 1], # V, along a, shifted in a and b
+                         [-0.25*shift_a, -0.25*shift_b,	 0.00*shift_c, 1, 1], # V, along a, shifted in a and b
+                         [ 0.00*shift_a,  0.25*shift_b,  0.25*shift_c, 1, 1], # V, along c, shifted in b and c
+                         [ 0.00*shift_a,  0.25*shift_b, -0.25*shift_c, 1, 1]]) # V, along c, shifted in b and c
+
+jump_atoms = np.array([0,1,4,5]) # List of atoms considered as N nearest neighbours,
+                                 # from the original atomic base
--- EFG_point_charge.py
+++ EFG_point_charge.py
@@ -1,30 +1,34 @@
-# This is the main file, where we import the basis from
-# 'Crystalline_structure.py' and the functions that calculate the EFG from point charge
-# from 'EFG_functions.py'
+# -*- coding: utf-8 -*-
+"""
+@author: Jonathan Frassineti
+"""
+
+""" 
+This is the main file, where we import the basis from
+'Crystalline_structure.py' and the functions that calculate the EFG from point charge
+from 'EFG_functions.py'.
+"""
 
 import numpy as np
-from EFG_functions import diagonalize_EFG, point_charge_EFG
+from EFG_functions import point_charge_EFG, diagonalize_EFG
 
-atomic_EFG = - 9.71736166e21 # V/(m^2), atomic unit of EFG
-angtom = 1.0e-10 # Angstrom unit of measure
-elementary_charge = 1.6021766e-19 # Coulomb = ampere*second
-epsilon0 = 8.8541878e-12 # dielectric constant of the vacuum, in ampere^2*kilogram^−1*meter^−3*second^4
-h = 6.6260693e-34 # J*s
-hbar = h/(2*np.pi) # J*s
 
-lattice_par = 4.722 # Lattice parameter, in Angstrom
-quadrupole_moment = -0.052e-28 # Quadrupolar moment of the atom, in m^2
-
-EFG_original = point_charge_EFG(lattice_par,np.array([0.25, 0.00, 0.50]),10)
-EFG_diag, eta, chi = diagonalize_EFG(EFG_original,quadrupole_moment)
+"""
+Now, in function 'EFG_original()' you have to put the atomic position
+where you want to calculate the EFG, the number of steps of the algorithm
+(default, s = 15) and if you want to add an impurity (default, impurity = True).
+"""
+ 
+EFG_original = point_charge_EFG(np.array([0.00, 0.25, -0.25]), s = 15, impurity = False)
+EFG_diag, eta, chi = diagonalize_EFG(EFG_original)
 
 print("The original point charge EFG is: ")
 print(EFG_original)
-print("V/(m^2)")
+print("V/(m^2)\n\n")
 
 print("The diagonalized point charge EFG is: ")
 print(EFG_diag)
-print("V/(m^2)")
+print("V/(m^2)\n\n")
 
 print("The EFG parameters are: ")
 print("eta = ",eta)
